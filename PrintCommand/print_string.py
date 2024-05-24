a86ca9464951cf4420a81df0dfd7db92b38749f6
Piya-Boy <87295280+Piya-Boy@users.noreply.github.com>
2024-05-24 15:48:38 +0700

    ex_01

---
+++ PrintCommand/print_string.py
@@ -0,0 +1,44 @@
+print("Hello")
+print("Hello World")
+print("Hello_python")
+print("Merry X\'mas")
+print('i want to ask you "Why don\'t you drive to work ?"')
+print('"I don\'t have a car"')
+print("You got a new job !? That\'s so exciting")
+print('สวัสดีวันจันทร์ความแตกต่างระหว่างคนเก่งกับคนไม่เก่ง คือ \n "การใช้เวลาว่างให้เป็นประโยชน์"')
+print("\\/\/\/\\")
+print(
+    '''
+    a
+    an
+    ant
+    '''
+)
+print(
+    '''
+        *
+    *   *   *
+        *
+    '''
+)
+print(
+    '''
+    *   +   *
+    +   *   +
+    *   +   *
+    '''
+)
+print(
+    '''
+    just because something
+    thinks differently from you,
+    does that mean it\'s not thinking?
+    '''
+)
+print(
+    '''
+    \       /
+        X   
+    /       \
+    '''
+)
