e586e0106de07bad3cd836b8aa6bb2b4327d03ce
DESKTOP-QSKE9IR\HP Victus 15 <mynest.classic@gmail.com>
2024-05-24 15:24:19 +0700

    test

---
+++ python/print_string.py
@@ -0,0 +1,26 @@
+print ('1. Hello')
+print ('2. Hello World')
+print ('3. Hello_python')
+print ('4. Marry X\'Mas')
+print ('5. I want to ask you \" Why don\'t you drive to work \"')
+print ('6. \" I don\'t have car \"')
+print ('7. You got a new job !? That\'s so exciting ')
+print ( '8. สวัสดีวันจันทร์')
+print ( '9. ความแตกต่างระหว่างคนเก่งกับคนไม่เก่ง คือ \"การใช้เวลาว่างให่้เป็นประโชนย์\"')
+print ('10. /\\/\\/\\/\\')
+print ('''11. a
+    an
+    ant''')
+print ('''12.     *       
+    *   *   *
+        *''')   
+print ('''13.    *    +   *
+       +    *   +
+       *    +   *''')
+print ("""14. just because something
+thinks differently from you,
+does that mean it\'s not thinking?""")
+print ('''15. \\        /
+        X
+    /        \\
+''')
