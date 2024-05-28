e3777e981bf06a54b49d368dd7b7174481ecc47e
mihawk18 <phornsin.amp@gmail.com>
2024-05-28 12:47:52 +0700

    ex01

---
+++ PrintCommand/print_string.py
@@ -0,0 +1,35 @@
+print("Hello")
+print("Hello World")
+print("Hello_python")
+print("Merry X'Mas")
+print("i want to ask you \"Why don\' t you drive to work ?\"")
+print("\"I don\'t have a car\"")
+print("You got a new job !? That's so exciting")
+print("สวัสดีวันจันทร์")
+print("ความแตกต่างระหว่างคนเก่งกับคนไม่เก่ง คือ \"การใช้เวลาว่างให้เป็นประโยชน์\"")
+print("/\\/\\/\\")
+print("""
+      a
+      an
+      ant
+      """)
+print("""
+          *   
+      *   *   *
+          *   
+      """)
+print("""
+      *   +   *
+      +   *   +
+      *   +   * 
+      """)
+print("""
+    just because something
+    thinks differently from you,
+    does that mean it's not thinking?
+      """)
+print("""
+      \       /
+          X   
+      /       \ 
+      """)
