235ef4fa786f8827f5de68cf4b9f86ef91664cf2
W1b00N <123526536+W1b00N@users.noreply.github.com>
2024-05-31 14:52:36 +0700

    add folder variable

---
+++ Variable/variable_numberic.py
@@ -0,0 +1,5 @@
+a = 5
+b = 7
+print("a = ", a)
+print("b = ", b)
+print("a + b = ", a + b)
---
+++ Variable/variable_string.py
@@ -0,0 +1,7 @@
+firstname = "John"
+lastname = "doe"
+age_full = "I'm 32 years old."
+# print(firstname, lastname, age_full)
+print("My firstname is " + firstname + ".")
+print("My lastname is " + lastname + ".")
+print(age_full)
