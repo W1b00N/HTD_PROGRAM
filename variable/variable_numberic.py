29d73f7a53b8400966a2b7f644ac17186333206e
Kid <>
2024-05-31 14:19:32 +0700

    no message

---
+++ variable/variable_numberic.py
@@ -0,0 +1,6 @@
+a = 5
+b = 7
+print("a = ", a)
+print("b = ", b)
+print("a + b = ", a+b)
+
---
+++ variable/variable_string.py
@@ -0,0 +1,7 @@
+firstname = "สมคิด"
+lastname = "นายนาม"
+age_Full = 'I\' 24 ปี'
+print(firstname, lastname, age_Full)
+print('My first name is' + firstname +'.')
+print('My last name is' + lastname +'.')
+print('age_Full' + age_Full +'.')
