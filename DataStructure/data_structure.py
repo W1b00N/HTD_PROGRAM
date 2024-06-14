fbd5ad96c81fb248ec48afdd65ef2c8cb72da272
Piya-Boy <87295280+Piya-Boy@users.noreply.github.com>
2024-06-14 14:56:52 +0700

    ex03

---
+++ DataStructure/data_structure.py
@@ -0,0 +1,87 @@
+# 1
+numeric1 = 5
+print(numeric1)
+print(type(numeric1))
+
+# 2
+numeric2 = -3
+print(numeric2)
+print(type(numeric2))
+
+# 3
+numeric3 = 0
+print(numeric3)
+print(type(numeric3))
+
+# 4
+
+input1 = int(input())
+print(input1)
+print(type(input1))
+
+# 5
+numeric1 = 5.0
+print(numeric1)
+print(type(numeric1))
+
+# 6
+input2 = float(input())
+print(input2)
+print(type(input2))
+
+# 7
+int1 = int(input())
+int2 = int(input())
+print(int1 + int2)
+print(int1 - int2)
+print(int1 * int2)
+print(type(int1 + int2))
+print(type(int1 - int2))
+print(type(int1 * int2))
+
+# 8
+int1 = int(input())
+float1 = float(input())
+print(int1 + float1)
+print(int1 - float1)
+print(int1 * float1)
+print(type(int1 + float1))
+print(type(int1 - float1))
+print(type(int1 * float1))
+
+# 9
+logic1 = True
+logic2 = False
+print(logic1 or logic2)
+
+# List
+
+# 1
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[2])
+
+# 2
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[-3])
+
+# 3
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[0])
+
+# 4
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[1:-2])
+
+# 5
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[1:4])
+
+# 6 
+list1 = []
+str1 = input()
+str2 = input()
+str3 = input()
+list1.append(str1)
+list1.append(str2)
+list1.append(str3)
+print(list1)
