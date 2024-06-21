a10d76fe7b32527e1895ebed545191bb794ba7d0
DESKTOP-QSKE9IR\HP Victus 15 <mynest.classic@gmail.com>
2024-06-21 16:00:10 +0700

    datastructure

---
+++ data_structure/List.py
@@ -0,0 +1,31 @@
+
+list1 = [0, 1, 2, 'a', 'b', 'c']
+list1.insert(2, 'd')   
+print(list1)
+
+
+list1 = [0, 1, 2, 'a', 'b', 'c']
+list1.insert(-3, 'd')
+print(list1)
+
+
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[0])
+
+
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[1:-2])
+
+
+list1 = [0, 1, 2, 'a', 'b', 'c']
+print(list1[1:5])
+
+
+list1 = []
+str1 = input()
+str2 = input()
+str3 = input()
+list1.append(str1)
+list1.append(str2)
+list1.append(str3)
+print(list1)
---
+++ data_structure/Numberic.py
@@ -0,0 +1,54 @@
+
+numeric1 = 5
+print(numeric1)
+print(type(numeric1))
+
+
+numeric2 = -3
+print(numeric2)
+print(type(numeric2))
+
+
+numeric3 = 0
+print(numeric3)
+print(type(numeric3))
+
+
+input1 = int(input())
+print(input1)
+print(type(input1))
+
+
+numeric1 = 5.0
+print(numeric1)
+print(type(numeric1))
+
+
+input2 = float(input())
+print(input2)
+print(type(input2))
+
+
+int1 = int(input())
+int2 = int(input())
+print(int1 + int2)
+print(int1 - int2)
+print(int1 * int2)
+print(type(int1 + int2))
+print(type(int1 - int2))
+print(type(int1 * int2))
+
+
+int1 = int(input())
+float1 = float(input())
+print(int1 + float1)
+print(int1 - float1)
+print(int1 * float1)
+print(type(int1 + float1))
+print(type(int1 - float1))
+print(type(int1 * float1))
+
+
+logic1 = True
+logic2 = False
+print(logic1 or logic2)
